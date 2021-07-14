addc09bac8e38eb0bdae74511ee364e80e8bdf30
Luis Hernández Piney <luis.piney@gmail.com>
2021-07-14 19:33:40 +0200

    Add Enumeration test

--- Magnett.Automation.Core.Test/Common/Helpers/EnumerationMockup.cs
+++ Magnett.Automation.Core.Test/Common/Helpers/EnumerationMockup.cs
@@ -1,7 +1,16 @@
-﻿namespace Magnett.Automation.Core.Test.Common.Helpers
+﻿using Magnett.Automation.Core.Common;
+
+namespace Magnett.Automation.Core.Test.Common.Helpers
 {
-    public class EnumerationMockup
+    public class EnumerationMockup : Enumeration
     {
-        
+        public int GetId()
+        {
+            return Id;
+        }
+        public EnumerationMockup(int id, string name) :
+            base(id, name)
+        {
+        }
     }
 }
