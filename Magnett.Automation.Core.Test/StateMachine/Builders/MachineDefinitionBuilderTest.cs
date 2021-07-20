f3f0ff9c05d302e4fb5ea72bc877e62b57031e59
Luis Hernandez Piney <luis.piney@gmail.com>
2021-07-20 20:34:09 +0200

    Improve unit testing s2 (#14)
    * Complete State testing
    
    * Remove several Warnings
    
    * Create property test
    
    * Remove unused exception
    
    * Remove exceptions, serializable attribute
    
    * Add Transition Test
    
    * Add MachineDefinitionBuilder test

--- Magnett.Automation.Core.Test/StateMachine/Builders/MachineDefinitionBuilderTest.cs
+++ Magnett.Automation.Core.Test/StateMachine/Builders/MachineDefinitionBuilderTest.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using Xunit;
+﻿using Xunit;
 
 using Magnett.Automation.Core.StateMachine.Builders;
-using Magnett.Automation.Core.StateMachine.Exceptions;
+using Magnett.Automation.Core.Test.Common.Helpers;
 
 namespace Magnett.Automation.Core.Test.StateMachine.Builders
 {
     public class MachineDefinitionBuilderTest
     {
-        private const string InitialState = "Initial";
+        private const string InitialStateName = "InitialState";
+        private const string AnyStateName     = "AnyStateName";
 
         [Fact]
         public void Create_When_Invoke_Return_Instance()
@@ -16,8 +16,57 @@ namespace Magnett.Automation.Core.Test.StateMachine.Builders
             var instance = MachineDefinitionBuilder.Create();
 
             Assert.NotNull(instance);
+            Assert.IsType<MachineDefinitionBuilder>(instance);
+        }
+
+        [Fact]
+        public void InitialState_When_Invoke_With_String_Parameter_Return_Initial_StateBuilder()
+        {
+            var stateDefinition = MachineDefinitionBuilder
+                .Create()
+                .InitialState(InitialStateName);
+            
+            Assert.NotNull(stateDefinition);
+            Assert.IsType<StateBuilder>(stateDefinition);
+             Assert.True(stateDefinition.IsInitialState);
         }
         
+        [Fact]
+        public void InitialState_When_Invoke_With_Enumeration_Parameter_Return_Initial_StateBuilder()
+        {
+            var stateEnumeration = new EnumerationMockup(1, InitialStateName);
+            var stateDefinition = MachineDefinitionBuilder
+                .Create()
+                .InitialState(stateEnumeration);
+            
+            Assert.NotNull(stateDefinition);
+            Assert.IsType<StateBuilder>(stateDefinition);
+            Assert.True(stateDefinition.IsInitialState);
+        }
         
+        [Fact]
+        public void AddState_When_Invoke_With_String_Parameter_Return_Not_Initial_StateBuilder()
+        {
+            var stateDefinition = MachineDefinitionBuilder
+                .Create()
+                .AddState(AnyStateName);
+            
+            Assert.NotNull(stateDefinition);
+            Assert.IsType<StateBuilder>(stateDefinition);
+            Assert.False(stateDefinition.IsInitialState);
+        }
+        
+        [Fact]
+        public void AddState_When_Invoke_With_Enumeration_Parameter_Return_Not_Initial_StateBuilder()
+        {
+            var stateEnumeration = new EnumerationMockup(1, AnyStateName);
+            var stateDefinition = MachineDefinitionBuilder
+                .Create()
+                .AddState(stateEnumeration);
+            
+            Assert.NotNull(stateDefinition);
+            Assert.IsType<StateBuilder>(stateDefinition);   
+            Assert.False(stateDefinition.IsInitialState);
+        }
     }
 }
